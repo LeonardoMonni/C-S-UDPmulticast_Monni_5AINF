597b40901b05476404a5814d81049da153941b05
Monni Leonardo <monni@Len.station>
2024-04-23 10:47:55 +0200

    Implementazione classe Client (non completa)

--- src/clientserver_udpmulticast/ClientMulticastUDP.java
+++ src/clientserver_udpmulticast/ClientMulticastUDP.java
@@ -12,9 +12,61 @@ import java.net.*;
  * @author Monni Leonardo
  */
 public class ClientMulticastUDP {
+    //colore del Server
+     public static final String ANSI_PURPLE = "\u001B[35m";
+    //colore del client
+    public static final String ANSI_CYAN = "\u001B[36m";
+    //colore reset
+    public static final String RESET = "\033[0m";
+    //colore del 'gruppo'
+    public static final String ANSI_BLUE = "\u001B[34m";
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        //numero porta server
+        int porta = 2000;
+        //numero porta gruppo
+        int gruppoPorta = 1900;
+        //socket UDP
+        DatagramSocket dSocket = null;
+        //socket mutlicast UDP
+        MulticastSocket mSocket = null;
+        //indirizzo server
+        InetAddress serverAddress;
+        //indirizzo gruppo multicast UDP
+        InetAddress gruppo;
+        //Datagramma UDP con richiesta e risposta 
+        DatagramPacket outPacket, inPacket;
+
+        //buffer du lettura
+        byte[] inBuffer = new byte[256];
+        byte[] inBufferG = new byte[1024];
+
+        //messaggio di richiesta
+        String messageOut = "Richiesta comunicazione";
+        //messaggio di risposta
+        String messaggeIn;
+
+        /*try{
+        
+        
+        
+        
+     } catch (UnknownHostException ex) {
+           System.err.println(ex.getMessage());
+           
+       } catch (SocketException ex) {
+           System.err.println(ex.getMessage());   
+           
+       } catch (IOException ex) {
+           System.err.println(ex.getMessage());   
+       }   
+       finally{
+            if (dSocket != null)
+                dsocket.close();
+            if (mSocket != null)
+                msocket.close();
+
+        
+        }   */
     }
-    
 }
